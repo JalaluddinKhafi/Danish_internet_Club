70b7d5776674bd15ea4b77a350051cccaa5e6124
JalaluddinKhafi <jalaluddin.khafi@gmail.com>
2025-11-14 22:38:45 +0430

    Initial commit for Internet Club Danish project

---
+++ src/main/java/com/dahishclub/danish_internet_club/DanishInternetClubApplication.java
@@ -0,0 +1,13 @@
+package com.dahishclub.danish_internet_club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanishInternetClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanishInternetClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dahishclub/danish_internet_club/DanishInternetClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahishclub.danish_internet_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanishInternetClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
