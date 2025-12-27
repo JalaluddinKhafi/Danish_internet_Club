d9ad553c460d33051a00c04c63deb741a2162f62
JalaluddinKhafi <khafi98117@gmail.com>
2025-12-27 10:34:00 +0430

    attachment's attributes are added

--- src/main/java/com/dahishclub/danish_internet_club/model/Attachments.java
+++ src/main/java/com/dahishclub/danish_internet_club/model/Attachments.java
@@ -1,4 +1,19 @@
 package com.dahishclub.danish_internet_club.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.io.File;
+import java.util.Date;
+
+@Data
+@Entity
 public class Attachments {
+    @Id
+    Long id;
+    String title;
+    String description;
+    File attachment;
+    Date created_at;
 }
