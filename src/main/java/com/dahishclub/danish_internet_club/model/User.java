56bb1b0ca00f982465917ac5bb95ffccaafa2bee
JalaluddinKhafi <khafi98117@gmail.com>
2025-12-27 10:29:24 +0430

    User's attributes are added

--- src/main/java/com/dahishclub/danish_internet_club/model/User.java
+++ src/main/java/com/dahishclub/danish_internet_club/model/User.java
@@ -1,6 +1,21 @@
 package com.dahishclub.danish_internet_club.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class User {
 
+
+    @Id
+    private Long id;
+    String fullname;
+    String username;
+    String password;
+    String email;
+    String phone;
+    String address;
 
 }
