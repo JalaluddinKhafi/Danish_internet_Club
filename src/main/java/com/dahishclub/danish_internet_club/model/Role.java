2ee1af4ce5921691b479ba894e7302c37ef50b53
JalaluddinKhafi <khafi98117@gmail.com>
2025-12-27 10:37:09 +0430

    role completed

--- src/main/java/com/dahishclub/danish_internet_club/model/Role.java
+++ src/main/java/com/dahishclub/danish_internet_club/model/Role.java
@@ -1,4 +1,24 @@
 package com.dahishclub.danish_internet_club.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Role {
+    @Id
+    Long id;
+    String name;
+    String description;
+
+    public Role(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Role() {
+
+    }
 }
